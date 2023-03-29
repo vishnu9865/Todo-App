2a0f651fa97a955cb2cd7f62e3f74be272bf5878
Jeyavishnu S <sjeyavishnu9865@gmail.com>
2023-03-29 10:33:11 +0530

    edited vite.config.js file to include base url.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/Todo-App/',         // this is the base url in the format /repo-name/ for hosting in github pages
+  plugins: [react()]
 })
